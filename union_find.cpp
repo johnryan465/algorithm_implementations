3ca9be2077e36065f3613dac42e5fc606c9dc6f6
John Ryan <johnryan465@gmail.com>
2017-02-24 19:55:27 +0000

    Update union_find.cpp

--- union_find.cpp
+++ union_find.cpp
@@ -4,20 +4,22 @@
 using namespace std;
 
 struct UF{
-	int cnt;
-	vector<int> id,sz;
+    int cnt;
+    vector<int> id,sz;
     UF(int N)   {
-        cnt = N;
-		id.resize(N);
-		sz.resize(N);
-        for(int i=0; i<N; i++)	{
-            id[i] = i;
-	    	sz[i] = 1;
-		}
+	    cnt = N;
+	    id.resize(N);
+	    sz.resize(N);
+	    for(int i=0; i<N; i++)	{
+		    id[i] = i;
+		    sz[i] = 1;
+	    }
     }
     int find(int p)	{
         int root = p;
-        while (root != id[root]){root = id[root];}
+        while (root != id[root]){
+		root = id[root];
+	}
         while (p != root) {
             int newp = id[p];
             id[p] = root;
@@ -30,12 +32,11 @@ struct UF{
         int j = find(y);
         if (i == j) return;
         if (sz[i] < sz[j]){ 
-			id[i] = j; 
-			sz[j] += sz[i]; 
-		} else	{ 
-			id[j] = i; 
-			sz[i] += sz[j]; 
-		}
+		id[i] = j; 
+		sz[j] += sz[i]; 
+	} else	{ 
+		id[j] = i; sz[i] += sz[j]; 
+	}
         cnt--;
     }
     bool connected(int x, int y){return find(x) == find(y);}
