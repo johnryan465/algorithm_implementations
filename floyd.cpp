0c0ee22f33438bb764e554cb5e921a8322e11ca1
John Ryan <johnryan465@gmail.com>
2017-04-19 13:09:45 +0000

    Add files via upload

---
+++ floyd.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int dist[100][100];
+void flyod(){
+	for(int k = 0; k < n; k++){
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
+			}
+		}
+	}
+}
+int main(){
+
+}
